7dcf9516a3ec7686a8bc056344366d51c6515535
adamijan <a.damijan@t-online.de>
2025-04-08 08:40:32 -0400

    final work

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -73,18 +73,47 @@ public_users.get('/title/:title',function (req, res) {
   }
 });
 
-//  Get book review
-public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  //return res.status(300).json({message: "Yet to be implemented"});
-  const isbn = req.params.isbn; 
+// Task 10: 
+public_users.get('/books', async (req, res) => {
+    try {
+        const response = await axios.get('http://localhost:5000/'); 
+        res.json(response.data);
+    } catch (error) {
+        res.status(500).json({ message: 'Error fetching booklist' });
+    }
+});
 
-  if (books[isbn]) {
-      const reviews = books[isbn].reviews;
-      res.status(200).send(JSON.stringify(reviews, null, 4));
-  } else {
-      res.status(404).json({ message: "No reviews found by this ISBN" });
-  }
+// Task 11: Get Book Details Based on ISBN
+public_users.get('/books/:isbn', async (req, res) => {
+    const { isbn } = req.params;
+    try {
+        const response = await axios.get(`http://localhost:5000/isbn/${isbn}`); 
+        res.json(response.data);
+    } catch (error) {
+        res.status(500).json({ message: 'Error fetching book details' });
+    }
+});
+
+// Task 12: 
+public_users.get('/books/author/:author', async (req, res) => {
+    const { author } = req.params;
+    try {
+        const response = await axios.get(`http://localhost:5000/author/${author}`); 
+        res.json(response.data);
+    } catch (error) {
+        res.status(500).json({ message: 'Error fetching books by author' });
+    }
+});
+
+// Task 13: 
+public_users.get('/books/title/:title', async (req, res) => {
+    const { title } = req.params;
+    try {
+        const response = await axios.get(`http://localhost:5000/title/${title}`); 
+        res.json(response.data);
+    } catch (error) {
+        res.status(500).json({ message: 'Error fetching books by title' });
+    }
 });
 
 module.exports.general = public_users;
